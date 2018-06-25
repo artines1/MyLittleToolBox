a709a68111eebb2b9f02ad805eb5a0bd880d79c0
Tim Huang <tihuang@mozilla.com>
2018-06-25 22:08:31 +0200

    Making the script can honor the modified path.

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ git submodule update --init --recursive
 echo "Starting setting bash environment ..."
 ./setting_bash.sh
 
+# Update the PATH ...
+source $HOME/.bashrc
+
 echo "Starting setting git environment ..."
 ./setting_git.sh
 
